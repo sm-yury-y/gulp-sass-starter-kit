ff5bb12ad3e11255e75091612c75adb75ab7dd98
sm-yury-y <sm.yuri-y@yandex.ru>
2019-04-12 21:42:02 +0300

    Добавляет таск для компиляции sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,38 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
-// Compile Sass
-gulp.task('sass', function(){
-  return gulp.src('source/styles/*.scss')
-    .pipe(sass().on('error', sass.logError))
+
+/* Styles compile */
+gulp.task('sass', function() {
+  return gulp.src('source/styles/main.scss')
+    .pipe(plumber({
+      errorHandler: notify.onError((err) => {
+        return {
+          title: 'Styles',
+          message: err.message
+        }
+      })
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    // .pipe(rename({
+    //   suffix: '.min',
+    //   prefix: ''
+    // }))
+    // .pipe(cleanCSS())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/css'));
 });
